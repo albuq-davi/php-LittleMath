9229ac3007df08ee8ef74a8b67d440a415091f29
Davi Paiva <davsalbuquerque@gmail.com>
2025-02-22 01:28:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Little Math</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Little Math Version 02</h1>
+    <hr>
+    
+</body>
+</html>
+
+<!--PHP PORTION-->
+<?php
+  $num01 = 5;
+  $num02 = 2;
+  $sum = $num01 + $num02;
+  $sub = $num01 - $num02;
+  $mult = $num01 * $num02;
+  $division = $num01 / $num02;
+  
+
+  echo "The numbers are $num01 and $num02";
+  echo "
+        <ul>
+            <li>$sum</li>
+            <li>$sub</li>
+            <li>$mult</li>
+            <li>$division</li>
+        </ul>
+    ";
+
+?>
+
+
+
+
+
+
